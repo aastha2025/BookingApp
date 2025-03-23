f1645c5ec78970114a7972763c4ba4a9127f55e0
Astronaut004 <ac4548817@gmail.com>
2025-03-23 16:25:04 +0530

    Add Routing

---
+++ BookingApp/src/Component/MyHome.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Home from "./Home";
+import Offers from './Offers';
+import Property from './Property';
+import Destination from './Destination';
+import Trip from './Trip';
+import India from './India';
+import Stay from './Stay';
+import Homelove from './Homelove';
+import Unique_prop from './Unique_prop';
+import Inspire_trip from './Inspire_trip';
+
+function App() {
+  return (
+    <div>
+      <Home />
+      <Offers/>
+      <Property />
+      <Destination />
+      <Trip />
+      <India />
+      <Stay />
+      <Homelove />
+      <Unique_prop />
+      <Inspire_trip />
+    </div>
+  );
+}
+
+export default App;
