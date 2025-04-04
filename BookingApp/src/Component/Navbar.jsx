01c3daed70eff232b95f1913c21482092f931975
Astronaut004 <ac4548817@gmail.com>
2025-04-04 09:46:39 +0530

    Hamburger

--- BookingApp/src/Component/Navbar.jsx
+++ BookingApp/src/Component/Navbar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="container">
@@ -9,23 +11,25 @@ function Navbar() {
                     <div className="logo">
                         <a href="/">BookingApp</a>
                     </div>
-                    <div className="nav-right">
+                    <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                        ☰
+                    </button>
+                    <div className={`nav-right ${menuOpen ? "open" : ""}`}>
                         <button className="nav-item">INR</button>
-                        <button className="nav-item"><img src="" alt="india" /></button>
+                        <button className="nav-item"><img src="/images/india.png" alt="india" className='icons' /></button>
                         <button className="nav-item"><img src="/images/icons8-unverified-account-50.png" alt="ques" className='icons' /></button>
                         <button className="nav-item">List your property</button>
                         <button className="login">Register</button>
                         <button className="login">Sign In</button>
                     </div>
                 </div>
-                <div className="lowernavbar">
-                    <button className="nav-a"><img src="/images/icons8-bed-50.png" alt="Stays" className='nav-img' />Stays</button>
-                    <button className="nav-a"> <img src="/images/icons8-plane-50.png" alt="flights" className='nav-img' /> Flights</button>
-                    <button className="nav-a"> <img src="/images/icons8-plane-50.png" alt="flights + hotels" className='nav-img' /> Flight + Hotel</button>
-                    <button className="nav-a"> <img src="/images/icons8-car-30.png" alt="car rental" className='nav-img' /> Car rental</button>
-                    <button className="nav-a"> <img src="/images/icons8-business-network-50.png" alt="attraction" className='nav-img' /> Attractions</button>
-                    <button className="nav-a"> <img src="/images/icons8-airport-50.png" alt="airport taxi" className='nav-img' /> Airport taxis</button>
-
+                <div className={`lowernavbar ${menuOpen ? "open" : ""}`}>
+                    <button className="nav-a"><img src="/images/icons8-bed-50.png" alt="Stays" className='nav-img' /> Stays</button>
+                    <button className="nav-a"><img src="/images/icons8-plane-50.png" alt="flights" className='nav-img' /> Flights</button>
+                    <button className="nav-a"><img src="/images/icons8-plane-50.png" alt="flights + hotels" className='nav-img' /> Flight + Hotel</button>
+                    <button className="nav-a"><img src="/images/icons8-car-30.png" alt="car rental" className='nav-img' /> Car rental</button>
+                    <button className="nav-a"><img src="/images/icons8-business-network-50.png" alt="attraction" className='nav-img' /> Attractions</button>
+                    <button className="nav-a"><img src="/images/icons8-airport-50.png" alt="airport taxi" className='nav-img' /> Airport taxis</button>
                 </div>
             </div>
         </nav>
