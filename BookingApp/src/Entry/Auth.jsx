9725c80ff15baadd0e27bce49b5e8642b689e107
aastha <aastha2000sh@gmail.co>
2025-04-09 00:42:43 +0530

    done

--- BookingApp/src/Entry/Auth.jsx
+++ BookingApp/src/Entry/Auth.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import "./Auth.css";
 function Auth(){
     return (
-        <div>hi</div>
+        <div>
+            <div>
+                <h3>Sign in or create an account</h3>
+                <p>You can sign in using your Booking.com account to access our services.</p>
+                
+            </div>
+        </div>
     )
 }
 export default Auth;
