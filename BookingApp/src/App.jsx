26c0165a495daf8887fec8c0d3959e7db0bf22d7
Astronaut004 <ac4548817@gmail.com>
2025-04-09 01:07:44 +0530

    done

--- BookingApp/src/App.jsx
+++ BookingApp/src/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MyHome from './Component/MyHome'
 import Navbar from './Component/Navbar';
-
+import Flight from './Component/Flight/Flight'
+import Authy from './Entry/Auth'
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Navbar />
         <Routes>
           <Route path='/' element={<MyHome />} />
+          <Route path='/flight' element={<Flight />} />
+          <Route path='/auth' element={<Authy />} />
         </Routes>
       </Router>
     </div>
