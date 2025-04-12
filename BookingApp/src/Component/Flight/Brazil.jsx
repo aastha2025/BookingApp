58ac923bf7d5234e5bef99aa7ac2d4261cd69514
Astronaut004 <ac4548817@gmail.com>
2025-04-13 01:34:57 +0530

    Add Brazil cards in Flight

---
+++ BookingApp/src/Component/Flight/Brazil.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import style from './Brazil.module.css'
+const Brazil = (props) => {
+    return (
+        <div className={style.cardContainer}>
+            <img className={style.BrazilImg} src={props.sr} alt="" width={"350px"} height={"200px"} />
+            <div className={style.brazilContent}>
+                <span className={style.head}>{props.head}</span>
+                <span className={style.subHeading}>{props.subHead}</span>
+                <span className={style.brazilP}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem ab molestiae deleniti fugit rem vel.</span>
+            </div>
+        </div>
+    )
+}
+
+export default Brazil
