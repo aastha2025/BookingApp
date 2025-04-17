16f9f22732a35ed6c55113df8babf4eeca27b9ad
Astronaut004 <ac4548817@gmail.com>
2025-04-17 12:55:13 +0530

    Flight slick and grid

--- BookingApp/src/Component/CorouSlick/TopFlightGrid.jsx
+++ BookingApp/src/Component/CorouSlick/TopFlightGrid.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './TopFlightGrid.module.css';
 
-const TopFlightGrid = ({ items }) => {
+const TopFlightGrid = ({ item }) => {
   return (
     <div className={styles.gridContainer}>
-      {items.map((item, index) => (
+      {item.map((item, index) => (
         <div key={index} className={styles.gridItem}>
           <img src={item.image} alt={`Flight Route ${index + 1}`} className={styles.image} />
           <span className={styles.txy}>{item.text}</span>
