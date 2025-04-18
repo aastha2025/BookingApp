db10e3530a1937461d3f142fb7685aa7e3aac6bd
aastha <aastha2000sh@gmail.co>
2025-04-19 02:08:36 +0530

    made car page

---
+++ BookingApp/src/Component/Car/data1.js
@@ -0,0 +1,23 @@
+export  const Cities = [
+    { id: "1" ,image: "/images/City1.jpg", name: "El Segundo", loc: "103 car rental locations", price: "Average price of ₹ 5,217.46 per day" },
+    { id: "2" ,image: "/images/City2.jpg", name: "Dania Beach", loc: "92 car rental locations", price: "Average price of ₹ 3,929.20 per day" },
+    { id: "3" ,image: "/images/City3.jpg", name: " Coolangatta", loc: "22 car rental locations", price: "Average price of ₹ 4,512.57 per day" },
+    { id: "4" ,image: "/images/City4.jpg", name: "Phoenix", loc: "78 car rental locations", price: "Average price of ₹ 5,498.81 per day" },
+    { id: "5" ,image: "/images/City5.jpg", name: "Jamaica", loc: "79 car rental locations", price: "Average price of ₹ 6,796.36 per day" },
+    { id: "6" ,image: "/images/City6.jpg", name: "Irving", loc: "81 car rental locations", price: "Average price of ₹ 5,168.07 per day" },
+    { id: "7" ,image: "/images/City7.jpg", name: "Madrid", loc: "108 car rental locations", price: "Average price of ₹ 4,150.51 per day" },
+    { id: "8" ,image: "/images/City8.jpg", name: "Calgary", loc: "46 car rental locations", price: "Average price of ₹ 5,493.13 per day" },
+    { id: "9" ,image: "/images/City9.jpg", name: "San Diego", loc: "87 car rental locations", price: "Average price of ₹ 4,688.26 per day" }
+];
+
+export  const Airports = [
+    { id: "1" ,image: "/images/Air1.jpg", name: "Palma de Mallorca Airport", loc: "41 car rental locations", price: "Average price of ₹ 2,804.90 per day" },
+    { id: "2" ,image: "/images/Air1.jpg", name: "Orlando International Airport", loc: "122 car rental locations", price: "Average price of ₹ 3,829.19 per day" },
+    { id: "3" ,image: "/images/Air1.jpg", name: " Faro Airport", loc: "43 car rental locations", price: "Average price of ₹ 2,607.71 per day" },
+    { id: "4" ,image: "/images/Air1.jpg", name: "Malaga Airport", loc: "54 car rental locations", price: "Average price of ₹ 2,740.95 per day" },
+    { id: "5" ,image: "/images/Air1.jpg", name: "Miami International Airport", loc: "115 car rental locations", price: "Average price of ₹ 4,651.76 per day" },
+    { id: "6" ,image: "/images/Air1.jpg", name: "Barcelona-El Prat Airport", loc: "51 car rental locations", price: "Average price of ₹ 3,291.56 per day" },
+    {id: "7" , image: "/images/Air1.jpg", name: "Humberto Delgado Airport", loc: "49 car rental locations", price: "Average price of ₹ 2,722.54 per day" },
+    { id: "8" ,image: "/images/Air1.jpg", name: "Ibiza Airport", loc: "37 car rental locations", price: "Average price of ₹ 2,765.02 per day" },
+    { id: "9" ,image: "/images/Air1.jpg", name: "Catania Fontanarossa Airport", loc: "63 car rental locations", price: "Average price of ₹ 3,534.20 per day" }
+];
