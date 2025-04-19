4f244a0cacf309067ff333419de57717e159dee5
Astronaut004 <ac4548817@gmail.com>
2025-04-19 19:33:13 +0530

    Add Attraction Link

--- BookingApp/src/Component/Navbar.jsx
+++ BookingApp/src/Component/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = React.memo(() => {
                     <Link to='/flight' className="nav-a"><img src="/images/icons8-plane-50.png" alt="flights" className='nav-img' /> Flights</Link>
                     <Link to="/hotel" className="nav-a"><img src="/images/icons8-plane-50.png" alt="flights + hotels" className='nav-img' /> Flight + Hotel</Link>
                     <Link to="/car" className="nav-a"><img src="/images/icons8-car-30.png" alt="car rental" className='nav-img' /> Car rental</Link>
-                    <a className="nav-a"><img src="/images/icons8-business-network-50.png" alt="attraction" className='nav-img' /> Attractions</a>
+                    <Link to='/Attractions' className="nav-a"><img src="/images/icons8-business-network-50.png" alt="attraction" className='nav-img' /> Attractions</Link>
                     <a className="nav-a"><img src="/images/icons8-airport-50.png" alt="airport taxi" className='nav-img' /> Airport taxis</a>
                 </div>
             </div>
