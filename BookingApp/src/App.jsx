ed63c235433364cef9d14c04410511db14ddba0c
Astronaut004 <ac4548817@gmail.com>
2025-05-01 00:47:37 +0530

    Child Data files

--- BookingApp/src/App.jsx
+++ BookingApp/src/App.jsx
@@ -1,12 +1,11 @@
 import React, { Suspense, lazy } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, useLocation, matchPath } from 'react-router-dom'
 import Navbar from './Component/Navbar'
 import './App.css'
 import MyHome from './Component/MyHome'
 import Footer1 from './Component/Footer1'
 import './Component/Flight/global.module.css';
 import Taxi from "./Component/Taxi/Taxi";
-
 
 // Lazy-loaded pages
 const Flight = lazy(() => import('./Component/Flight/Flight'))
@@ -16,7 +15,7 @@ const Hotel = lazy(() => import('./Component/Hotels/Hotel'))
 const NotFound = lazy(() => import('./Component/Errory/NotFound'))
 const Car = lazy(() => import('./Component/Car/Car'))
 const Attraction  = lazy(() => import('./Component/Attraction/Attraction'));
-
+const ChildPage = lazy(() => import('./Component/ChildPages/PageDetail'));
 
 function App() {
   return (
@@ -28,13 +27,28 @@ function App() {
 
 function AppLayout() {
   const location = useLocation()
+  
   // Define routes where footers should be hidden
   const noFooterRoutes = ['/hotel']
   const hideFooter = noFooterRoutes.includes(location.pathname.toLowerCase())
+  
+  // Check if current route is the NotFound page by matching against all defined routes
+  const isNotFoundPage = ![
+    '/',
+    '/flight',
+    '/auth',
+    '/otp',
+    '/hotel',
+    '/car',
+    '/Attractions',
+    '/taxi',
+    '/child'
+  ].some(route => matchPath(location.pathname, route))
 
   return (
     <>
-      <Navbar />
+      {/* Don't show navbar for NotFound page */}
+      {!isNotFoundPage && <Navbar />}
 
       <Suspense fallback={<div id="loader"></div>}>
         <Routes>
@@ -44,19 +58,15 @@ function AppLayout() {
           <Route path='/otp' element={<Otp />} />
           <Route path='/hotel' element={<Hotel />} />
           <Route path='*' element={<NotFound />} />
-          <Route  path= "/car"  element={<Car/>}/>
-          <Route  path= "/Attractions"  element={<Attraction />}/>
-          <Route  path= "/taxi"  element={<Taxi/>}/>
+          <Route path="/car" element={<Car/>}/>
+          <Route path="/Attractions" element={<Attraction />}/>
+          <Route path="/taxi" element={<Taxi/>}/>
+          <Route path='/child' element={<ChildPage />} />
         </Routes>
       </Suspense>
 
-      {/* Conditionally render footers */}
-      {!hideFooter && (
-        <>
-          {/* <Footer /> */}
-          <Footer1 />
-        </>
-      )}
+      {/* Conditionally render footers - hide for NotFound page and noFooterRoutes */}
+      {!isNotFoundPage && !hideFooter && <Footer1 />}
     </>
   )
 }
---
+++ BookingApp/src/Component/ChildPages/PageDetail.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import style from './PageDetail.module.css'
+import gb from '../Flight/global.module.css'
+
+const PageDetail = () => {
+  return (
+    <>
+    <div className={gb.container}>
+        <div className={style.flexy}>
+            <div className={style.l}></div>
+            <div className={style.r}>
+                <div className={style.card}>
+                    <img src="/booking.jpeg" alt="" />
+                    <div className={style.conty}></div>
+                </div>
+                <div className={style.card}></div>
+                <div className={style.card}></div>
+            </div>
+        </div>    
+    </div> 
+    </>
+  )
+}
+
+export default PageDetail
